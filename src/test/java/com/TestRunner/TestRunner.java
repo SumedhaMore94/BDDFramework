d75bf6d897cbad38190e1560bc6adafdc680407e
SumedhaMore <more94.sa@gmail.com>
2023-10-16 17:40:23 +0800

    Parallel execution code added

--- src/test/java/com/TestRunner/TestRunner.java
+++ src/test/java/com/TestRunner/TestRunner.java
@@ -3,6 +3,7 @@ package com.TestRunner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {"src/test/resources/Features"},
@@ -13,5 +14,10 @@ import io.cucumber.testng.CucumberOptions;
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 
 }
